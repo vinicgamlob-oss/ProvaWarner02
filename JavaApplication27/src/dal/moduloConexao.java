ec2c1c21f05c18795f5e26f1f8be72f9b487ae20
Vinicius Gama Bittencourt Lobo <vinicgamlob@gmail.com>
2026-05-20 19:30:06 -0300

    Update

---
+++ JavaApplication27/src/dal/moduloConexao.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+import java.sql.*;
+/**
+ *
+ * @author vinicius
+ */
+public class moduloConexao {
+    
+    
+    
+    public static Connection connector() {
+        
+       
+    java.sql.Connection conexao = null;
+    String driver = "com.mysql.cj.jdbc.Driver";
+    // Verifique se o nome do banco 'AulaDia2904' está idêntico no MySQL
+    String url = "jdbc:mysql://localhost:3306/AulaDia2904";
+    
+    String user = "root";
+    String password = "admin123";
+    
+    try {
+        Class.forName(driver);
+        conexao = DriverManager.getConnection(url, user, password);
+        return conexao; // Retorna a conexão se der certo
+    } catch (ClassNotFoundException | SQLException e) {
+        // Log do erro para você saber o que aconteceu se falhar
+        System.out.println("Erro na conexão: " + e);
+        return null; // Retorna null se der erro
+    }
+}
+
+    }
+
+    
+    
+    
+    
+
